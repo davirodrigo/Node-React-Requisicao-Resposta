13d93654878856b0a9e6b097137731e93c1dcbe2
davirodrigo <davirodrigopereira@gmail.com>
2025-06-12 09:32:52 -0300

    codigo comentado

---
+++ server.js
@@ -0,0 +1,11 @@
+/*OLÁ, Se você está vendo isso pelo meu github, prazer, este é o meu primeiro projeto node depois
+de um longo tempo após o ultimo... já fiz projetos como este anteriormente mas para voltar a
+pratica estarei comentando passo a passo meus feitos e do ambiente do código.
+
+git -v = me diz a versão do meu git
+git status = 
+git add . = adiciona todos os arquivos
+git commit -m "mensagem" = adiciona a mensagem ao commit
+git push -u origin master (ou main) = envia o commit para o github
+
+node -v */
